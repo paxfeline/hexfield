4de6f20ccbef3e22b1461ff76dcf61170c72150c
paxfeline <paxfeline@gmail.com>
2021-05-18 20:56:12 -0700

    files wip

--- builder-files.js
+++ builder-files.js
@@ -3,7 +3,7 @@ const builder_globals =
 	{
 		dragged: null,
 		file_sets: {
-			'default': ['index.html']
+			'default': ['index']
 		},
 		selected: 'default',
 		selectedFileIndex: 0,
@@ -16,6 +16,70 @@ function load_local_filesets()
 	const file_data = JSON.parse(file_data_str);
 	
 	builder_globals.file_data = file_data;
+	
+	const file_set = builder_globals.file_data.file_sets[ builder_globals.file_data.selected ];
+	
+	const file_list = document.querySelector('#file-list');
+	file_list.innerHTML = '';
+	
+	for (var i = 0; i < file_set.length; i++)
+	{
+		const div = create_file_div( file_set[i].name, i );
+		file_list.append(div);
+	}
+	
+	const sel_file = file_list.querySelector(`[data-file-index='${builder_globals.file_data.selectedFileIndex}']`);
+}
+
+function save_local_filesets()
+{
+	const file_data_str = JSON.stringify(builder_globals.file_data);
+	localStorage.setItem('HEXFIELD-FILE-DATA', file_data_str);
+}
+
+function change_select(new_ind)
+{
+	const file_list = document.querySelector('#file-list');
+}
+
+function load_selected_code()
+{
+	const file_set = builder_globals.file_data.file_sets[ builder_globals.file_data.selected ];
+	const file = file_set[ builder_globals.file_data.selectedFileIndex ];
+	
+	load_code( file.content );
+}
+
+function load_code(code)
+{
+	const parser = new DOMParser();
+	const doc = parser.parseFromString(code, "text/html");
+	
+	
+}
+
+function load_element(el)
+{
+	const type = el.tagName.toLowerCase();
+	const ne = document.querySelector(`[data-type='${type}']`).cloneNode(true);
+	
+	if (builder_globals.text_elements.includes(type))
+	{
+		const ta = ne.querySelector('textarea');
+		ta.innerHTML = el.innerHTML;
+	}
+	else if (builder_globals.known_elements.includes(type))
+	{
+		for (const chel in el.children)
+			ne.append(load_el(chel));
+		
+		if (el.children.length)
+		{
+			const botdz = document.createElement('div');
+			botdz.setAttribute('class', 'dropzone');
+			ne.append(botdz);
+		}
+	}
 }
 
 function create_new_file()
@@ -25,24 +89,24 @@ function create_new_file()
 	if (file_name)
 	{
 		const sel_set = builder_globals.selected;
-		const sel_ind = len( builder_globals.file_sets[ sel_set ] );
+		const sel_ind = builder_globals.file_sets[ sel_set ].length;
 		builder_globals.file_sets[ sel_set ].append(file_name);
 		builder_globals.selectedFileIndex = sel_ind;
 	
-		const 
+		
 	}
 }
 
-function create_file_div(name)
+function create_file_div(name, index)
 {
 	const templates = document.querySelector('#templates');
 	
 	const file_entry = templates.querySelector('.file-entry').cloneNode(true);
+	file_entry.setAttribute('data-file-index', index);
 	
-	if (name)
-	{
-		file_entry.querySelector('.file-name').innerHTML = name;
-	}
+	file_entry.querySelector('.file-name').innerHTML = name;
+	
+	return file_entry;
 }
 
 /*
--- builder-globals.js
+++ builder-globals.js
@@ -1,10 +1,12 @@
 const builder_globals =
 	{
+		text_elements: ['title', 'style', 'script', ],
+		known_elements: ['html', 'head', 'body', 'link', 'body', 'h1', 'div', 'span', 'img', 'p', 'nav', ],
 		dragged: null,
 		file_data:
 		{
 			file_sets: {
-				'default': ['index.html']
+				'default': [{name: 'index.html', content: ''}]
 			},
 			selected: 'default',
 			selectedFileIndex: 0,
