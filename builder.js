5fee3e34ed80c60ebbc0d40f78cd2d3d675d7417
paxfeline <paxfeline@gmail.com>
2021-05-22 01:21:12 -0700

    added trash counter for red drop effect

--- builder.js
+++ builder.js
@@ -194,20 +194,27 @@ function dragover_trash_handler(ev) {
 	}
 }
 
+var trash_in_out_count = 0;
+
 function onTrashDragLeave(event)
 {
-	console.log('trash leave');
+	trash_in_out_count--;
+	console.log('trash leave', trash_in_out_count);
 	const bank = document.querySelector('#bank');
-	bank.style.background = '';
-	bank.style.borderColor = '';
+	if (trash_in_out_count == 0)
+	{
+		bank.style.background = '';
+		bank.style.borderColor = '';
+	}
 }
 
 function onTrashDragEnter(event)
 {
-	console.log('trash enter');
-	event.preventDefault();
-	if (builder_globals.dragged)
+	trash_in_out_count++;
+	console.log('trash enter', trash_in_out_count);
+	if (builder_globals.dragged || builder_globals.dragged_attribute)
 	{
+		event.preventDefault();
 		const bank = document.querySelector('#bank');
 		bank.style.borderColor = 'darkred';
 		bank.style.background = 'red';
