030a30f4a42001d408b882bb8279593ec7873734
paxfeline <paxfeline@gmail.com>
2025-05-17 02:16:20 -0700

    switched CM lang to Python and allowed indentation

--- public/codemirror/editor.mjs
+++ public/codemirror/editor.mjs
@@ -1,11 +1,21 @@
 // see https://codemirror.net/examples/bundle/
-// node_modules/.bin/rollup public/codemirror/editor.mjs -f iife \
-//  -o editor.bundle.js -p @rollup/plugin-node-resolve
+/*
+node_modules/.bin/rollup public/codemirror/editor.mjs -f iife \
+  -o editor.bundle.js -p @rollup/plugin-node-resolve
+cp editor.bundle.js public/codemirror/
+
+*/
 
 import {EditorView, basicSetup} from "codemirror"
-import {javascript} from "@codemirror/lang-javascript"
+import {keymap} from "@codemirror/view"
+import {indentWithTab} from "@codemirror/commands"
+import {python} from "@codemirror/lang-python"
 
 let editor = new EditorView({
-  extensions: [basicSetup, javascript()],
+  extensions: [
+    basicSetup,
+    keymap.of([indentWithTab]),
+    python()
+  ],
   parent: document.body
 })
