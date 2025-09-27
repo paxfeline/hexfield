b00cfb3fca048608ccd47ffb5a8a6f7881d53c78
paxfeline <paxfeline@gmail.com>
2025-09-26 23:35:03 -0700

    wip

--- public/hex/tabs.js
+++ public/hex/tabs.js
@@ -147,28 +147,6 @@ class HexTabs extends HTMLElement
     
     if (tab_bodies.children.length > 0)
       this.selectedIndex = 0;
-
-    /////////
-    // Options for the observer (which mutations to observe)
-    const config = { attributes: false, childList: true, subtree: false };
-
-    // Callback function to execute when mutations are observed
-    const callback = (mutationList, observer) =>
-    {
-      for (const mutation of mutationList)
-      {
-        if (mutation.type === "childList")
-        {
-          console.log("A child node has been added or removed.");
-        }
-      }
-    };
-
-    // Create an observer instance linked to the callback function
-    const observer = new MutationObserver(callback);
-
-    // Start observing the target node for configured mutations
-    observer.observe(this, config);
   }
 }
 
