ce1d15e312b03918209e3e6cbe467e29d602e987
paxfeline <paxfeline@gmail.com>
2025-11-07 22:57:00 -0800

    added inverses... unnecessary?

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,4 +1,4 @@
 class Classroom < ApplicationRecord
-  belongs_to :creator, class_name: "Teacher"
+  belongs_to :creator, class_name: "Teacher", inverse_of: :classrooms
   has_many :teachers
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,3 @@
 class Teacher < User
-  has_many :classrooms, foreign_key: "creator_id"
+  has_many :classrooms, foreign_key: "creator_id", inverse_of: :creator
 end
