998595d50d43db00d4741ec2a95c5600dde12d9e
paxfeline <paxfeline@gmail.com>
2025-11-11 23:11:29 -0800

    commenting-out an unneeded migration

--- db/migrate/20251108003637_add_creator_to_students.rb
+++ db/migrate/20251108003637_add_creator_to_students.rb
@@ -1,5 +1,9 @@
-class AddCreatorToStudents < ActiveRecord::Migration[8.0]
-  def change
-    add_reference :users, :creator, null: true, foreign_key: { to_table: :users }
-  end
-end
+# TODO: remove if removing goes OK
+
+# handled by another migration
+
+# class AddCreatorToStudents < ActiveRecord::Migration[8.0]
+#   def change
+#     add_reference :users, :creator, null: true, foreign_key: { to_table: :users }
+#   end
+# end
